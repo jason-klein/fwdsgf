6a6d363232987e27eaea875916122fb03955bc0a
Jason Klein <jason.klein@logicforte.com>
2019-02-16 23:52:38 +0000

    Redirection Plugin Update

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '3.3.1' );
-define( 'REDIRECTION_BUILD', 'c2b766d11e00020c4a8cb5e565c6b2e7' );
+define( 'REDIRECTION_VERSION', '3.7.3' );
+define( 'REDIRECTION_BUILD', 'a878595a6024a1bf169ca4237eb6ffa7' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
